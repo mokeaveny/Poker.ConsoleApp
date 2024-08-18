72f7a46f9dd02e1562b0ab4b3f52802a4a605d73
Michael Keaveny <michaelkeaveny0@gmail.com>
2024-08-18 17:54:04 +0100

    Creation of the deck class and the associated unit tests

---
+++ Pocker.ConsoleApp.Tests/DeckTests.cs
@@ -0,0 +1,69 @@
+﻿using Poker.ConsoleApp.Classes;
+
+namespace Pocker.ConsoleApp.Tests
+{
+    [TestClass]
+    public class DeckTests
+    {
+        [TestMethod]
+        public void CreatesADeckOfFiftyTwoCorrectCards()
+        {
+            // Arrange
+            Deck deck = new Deck();
+            Dictionary<int, int> cardValueInstances = new Dictionary<int, int>();
+            Dictionary<Suits, int> suitInstances = new Dictionary<Suits, int>();
+
+            // Act
+            deck.Fill();
+
+            for (int i = 0; i < deck.GetAllCards().Count; i++)
+            {
+                Card currentCard = deck.GetAllCards()[i];
+                
+                if (cardValueInstances.ContainsKey(currentCard.Value) == false)
+                {
+                    cardValueInstances[currentCard.Value] = 1;
+                }
+                else
+                {
+                    cardValueInstances[currentCard.Value]++;
+                }
+
+                if (suitInstances.ContainsKey(currentCard.Suit) == false)
+                {
+                    suitInstances[currentCard.Suit] = 1;
+                }
+                else
+                {
+                    suitInstances[currentCard.Suit]++;
+                }
+            }
+
+            // Assert
+
+            // First check that there are 52 cards for a full deck
+            Assert.AreEqual(52, deck.GetAllCards().Count);
+
+            // Next check that there are 4 instances of each card value (2 - 14)
+            Assert.AreEqual(4, cardValueInstances[2]);
+            Assert.AreEqual(4, cardValueInstances[3]);
+            Assert.AreEqual(4, cardValueInstances[4]);
+            Assert.AreEqual(4, cardValueInstances[5]);
+            Assert.AreEqual(4, cardValueInstances[6]);
+            Assert.AreEqual(4, cardValueInstances[7]);
+            Assert.AreEqual(4, cardValueInstances[8]);
+            Assert.AreEqual(4, cardValueInstances[9]);
+            Assert.AreEqual(4, cardValueInstances[10]);
+            Assert.AreEqual(4, cardValueInstances[11]);
+            Assert.AreEqual(4, cardValueInstances[12]);
+            Assert.AreEqual(4, cardValueInstances[13]);
+            Assert.AreEqual(4, cardValueInstances[14]);
+
+            // Next check that there are 13 instances of each suit value (Diamonds, Hearts, Spades, Clubs)
+            Assert.AreEqual(13, suitInstances[Suits.Hearts]);
+            Assert.AreEqual(13, suitInstances[Suits.Diamonds]);
+            Assert.AreEqual(13, suitInstances[Suits.Spades]);
+            Assert.AreEqual(13, suitInstances[Suits.Clubs]);
+        }
+    }
+}
---
+++ Poker.ConsoleApp/Classes/Deck.cs
@@ -0,0 +1,39 @@
+﻿using Poker.ConsoleApp.Interfaces;
+
+namespace Poker.ConsoleApp.Classes
+{
+    public class Deck : IDeck
+    {
+        List<Card> Cards = new List<Card>();
+
+        public void Fill()
+        {
+            for (int i = 0; i < 52; i++)
+            {
+                Suits suit = (Suits)(Math.Floor((decimal)i / 13));
+                int value = i % 13 + 2;
+                Card newCard = new Card(value, suit);
+
+                this.Cards.Add(newCard);
+            }
+        }
+
+        public void Shuffle()
+        {
+            Random r = new Random();
+
+            for (int n = this.Cards.Count - 1; n > 0; --n)
+            {
+                int randomNumber = r.Next(n + 1);
+                Card temp = this.Cards[n];
+                this.Cards[n] = this.Cards[randomNumber];
+                this.Cards[randomNumber] = temp;
+            }
+        }
+
+        public List<Card> GetAllCards()
+        {
+            return this.Cards;
+        }
+    }
+}
